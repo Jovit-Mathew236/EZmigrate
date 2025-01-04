38fcf8b607efc3b91896bb97440d0de225d59ee0
Jovit-Mathew236 <jovitmathew236@gmail.com>
2025-01-04 15:53:21 +0530

    Refactor homepage video positioning and add 'Our Story' section with content and link to about page

--- components/madeups/Home/home.tsx
+++ components/madeups/Home/home.tsx
@@ -80,7 +80,7 @@ const HomePage = () => {
         autoPlay
         loop
         muted
-        className="absolute top-0 left-0 w-full h-screen object-cover -z-10"
+        className="fixed top-0 left-0 w-full h-screen object-cover -z-10"
       >
         <source src="/video/home.mp4" type="video/mp4" />
       </video>
@@ -397,6 +397,42 @@ const HomePage = () => {
               className="inline-flex items-center border border-black px-8 py-3 hover:bg-black hover:text-white transition-colors duration-300"
             >
               View more destinations
+              <svg
+                className="w-5 h-5 ml-2"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M17 8l4 4m0 0l-4 4m4-4H3"
+                />
+              </svg>
+            </Link>
+          </div>
+        </div>
+      </div>
+
+      {/* story section */}
+      <div className="relative z-10 bg-black md:bg-black/50 py-10 md:h-screen flex items-center justify-center">
+        <div className="container mx-auto px-6 md:flex flex-col items-center justify-center">
+          <h1 className="text-white text-4xl md:text-6xl font-light">
+            Our Story
+          </h1>
+          <p className="text-white mt-12 md:text-center text-xl md:text-4xl font-light max-w-4xl">
+            EZmigrate transforms dreams into global success. With expert
+            guidance and personalized support, we help students find the perfect
+            course, university, and destination, turning aspirations into
+            achievements.
+          </p>
+          <div className="mt-12">
+            <Link
+              href="/about"
+              className="inline-flex w-full md:w-auto justify-center items-center border border-white text-white px-16 py-4 hover:bg-white hover:text-black transition-colors duration-300"
+            >
+              About us
               <svg
                 className="w-5 h-5 ml-2"
                 fill="none"
